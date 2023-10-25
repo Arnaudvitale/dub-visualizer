79695aff636304c97e51a8a04c3a6dcf87c726ef
Arnaudvitale <91725889+Arnaudvitale@users.noreply.github.com>
2023-10-25 22:49:06 +0200

    [+] more designed interface and new features

--- dub-visualizer/src/preload.js
+++ dub-visualizer/src/preload.js
@@ -1,13 +1,63 @@
-
 document.addEventListener('DOMContentLoaded', function() {
     var myVideo = document.getElementById('myVideo');
     var videoSelector = document.getElementById('videoSelector');
     var recordButton = document.getElementById('microphone');
     var recordText = document.getElementById('recordText');
     var unrecordedText = document.getElementById('recordText1');
+    var videoContainer = document.getElementById('vid-cont');
     var mediaRecorder; // will be initialized with MediaRecorder instance
     var recordedChunks = [];
 
+    // Function to add a fade-in animation to an element
+    function addFadeInAnimation(element) {
+        element.classList.add('fade-in');
+        element.addEventListener('animationend', () => {
+            element.classList.remove('fade-in');
+        });
+    }
+
+    // Function to handle key presses
+    function handleKeyPress(event) {
+        switch(event.code) {
+            case 'ArrowLeft':
+                if (event.shiftKey) {
+                    myVideo.currentTime = Math.max(myVideo.currentTime - 0.1, 0);
+                } else {
+                    myVideo.currentTime = Math.max(myVideo.currentTime - 1, 0);
+                }
+                myVideo.pause();
+                break;
+            case 'ArrowRight':
+                if (event.shiftKey) {
+                    myVideo.currentTime = Math.min(myVideo.currentTime + 0.1, myVideo.duration);
+                } else {
+                    myVideo.currentTime = Math.min(myVideo.currentTime + 1, myVideo.duration);
+                }
+                myVideo.pause();
+                break;
+            case 'Space':
+                if (myVideo.paused) {
+                    myVideo.play();
+                } else {
+                    myVideo.pause();
+                }
+                event.preventDefault();
+                break;
+            case 'ArrowUp':
+                myVideo.currentTime = myVideo.duration;
+                event.preventDefault();
+                break;
+            case 'ArrowDown':
+                myVideo.currentTime = 0;
+                myVideo.pause();
+                event.preventDefault();
+                break;
+            default:
+                break;
+        }
+    }
+
+    // Function to handle video selection
     videoSelector.addEventListener('change', function(event) {
         var file = event.target.files[0];
         var fileURL = URL.createObjectURL(file);
@@ -16,6 +66,7 @@ document.addEventListener('DOMContentLoaded', function() {
         myVideo.style.display = 'block';
         recordButton.style.display = 'block';
         unrecordedText.style.display = 'block';
+        addFadeInAnimation(myVideo);
     });
 
     // Function to start recording
@@ -26,7 +77,8 @@ document.addEventListener('DOMContentLoaded', function() {
             // Reset previously recorded chunks
             recordedChunks = [];
             // Visual feedback for recording
-            recordButton.style.backgroundColor = 'red';
+            recordButton.style.border = 'none';
+            recordButton.style.filter = 'invert(100%)';
         };
 
         mediaRecorder.ondataavailable = (event) => {
@@ -38,7 +90,7 @@ document.addEventListener('DOMContentLoaded', function() {
         mediaRecorder.onstop = (event) => {
             // Create a blob from the recorded chunks
             const blob = new Blob(recordedChunks, {
-                type: 'audio/wav' // or 'audio/mp3' depending on browser capabilities
+                type: 'audio/wav'
             });
 
             // Create a link to download the audio
@@ -47,7 +99,7 @@ document.addEventListener('DOMContentLoaded', function() {
             document.body.appendChild(a);
             a.style = 'display: none';
             a.href = url;
-            a.download = 'enregistrement.wav'; // or 'enregistrement.mp3'
+            a.download = 'enregistrement.wav';
             a.click();
 
             // Cleanup
@@ -61,6 +113,7 @@ document.addEventListener('DOMContentLoaded', function() {
         mediaRecorder.start();
     }
 
+    // Function to handle recording
     recordButton.addEventListener('click', function() {
         // If we are not currently recording
         if (!mediaRecorder || mediaRecorder.state === 'inactive') {
@@ -70,6 +123,7 @@ document.addEventListener('DOMContentLoaded', function() {
                     // Start recording
                     myVideo.play();
                     recordText.style.display = 'block';
+                    addFadeInAnimation(recordText);
                     unrecordedText.style.display = 'none';
                     startRecording(stream);
                 })
@@ -81,7 +135,12 @@ document.addEventListener('DOMContentLoaded', function() {
             myVideo.pause();
             recordText.style.display = 'none';
             unrecordedText.style.display = 'block';
+            addFadeInAnimation(unrecordedText);
+            recordButton.style.border = '1px solid black';
+            recordButton.style.filter = 'invert(0%)';
             mediaRecorder.stop();
         }
     });
+    // Handle key presses
+    window.addEventListener('keydown', handleKeyPress);
 });
