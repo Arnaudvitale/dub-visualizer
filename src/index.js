90469f33288930ec4ffeb8262fb389d54c319be7
Arnaudvitale <arnaud.vitale@epitech.eu>
2023-12-04 11:51:50 +0100

    [+] enabled nodeIntegration

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ const createWindow = () => {
     width: 800,
     height: 600,
     webPreferences: {
+      nodeIntegration: true,
       preload: path.join(__dirname, 'preload.js'),
     },
   });
@@ -20,7 +21,7 @@ const createWindow = () => {
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
 };
 
 // This method will be called when Electron has finished
